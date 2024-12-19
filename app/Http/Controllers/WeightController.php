b3f64486e1d0b9694e5dbab0f17c58f860e18e90
andifraternal <andifraternal@gmail.com>
2024-12-19 13:04:19 +0700

    test

---
+++ app/Http/Controllers/WeightController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WeightController extends Controller
+{
+    // fungsi untuk menampilkan form input data timbangan
+    function input_weight(){
+        
+    }
+}
