fe4bf5809df4b083ef375cf3b950ea4631794267
andifraternal <andifraternal@gmail.com>
2025-01-14 12:46:54 +0700

    perbaikan-menu-outsole

--- app/Models/WeightModel.php
+++ app/Models/WeightModel.php
@@ -16,6 +16,14 @@ class WeightModel extends Model
     }
 
 
+    public static function get_data_model_name($model_name){
+        $data = collect(DB::select("
+            select top 1 * from master_data where model_name = '$model_name' and sf <> 0 and os <> 0 and ms <> 0  order by created_at asc
+        "))->first();
+        return $data;
+    }
+
+
     public static function get_data($lotid){
         $data = DB::selectResultSets("
             EXEC GET_DATA @LOTID = '$lotid'
@@ -31,9 +39,26 @@ class WeightModel extends Model
     }
 
 
+
+    public static function view_data_result_outsole($model_name){
+        $data = DB::select("
+            EXEC VIEW_PRODUCTION_RESULT_OUTSOLE @MODEL_NAME = '$model_name'
+        ");
+        return $data;
+    }
+
+
     public static function search_po($po_no){
         $data = collect(DB::select("
             SELECT PO_NO FROM [10.10.100.20].[MEShs].[dbo].[THPRODHISPO] with(nolock) WHERE PO_NO like '%$po_no' 
+        "));
+        return $data;
+    }
+
+
+    public static function search_model_name($model_name){
+        $data = collect(DB::select("
+            SELECT model_name FROM MASTER_DATA WHERE MODEL_NAME like '%$model_name%'  group by model_name
         "));
         return $data;
     }
